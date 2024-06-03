e3f98121ac9f38f1fc64f06cec692f942d6df5af
Taiye Jebutu <taiyejebutu@icloud.com>
2024-06-03 10:16:38 +0100

    Created solution

---
+++ EtherBridge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EtherBridge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
